5309debabe71ae3e33e8ee166649c065a5b4e0bb
Aidan Lane <aidanlane0@protonmail.com>
2020-08-21 22:45:40 -0400

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Aidan Lane on 8/21/20.
+//  Copyright © 2020 Aidan Lane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
